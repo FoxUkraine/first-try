916b8045f274c98bc4e5718f770240a9ca725711
FoxUkraine <51386049+FoxUkraine@users.noreply.github.com>
2019-12-17 11:12:19 +0200

    Add files via upload

---
+++ Юкин Лаба 1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       static int Checkout(int V)
+        {
+            return V;
+        }
+        static void Main(string[] args)
+        {
+            double V;
+            int R, h;
+            Console.WriteLine("Введіть радіус конуса");
+            Console.WriteLine("R=");
+            R = Convert.ToInt32(Console.ReadLine());
+            R = Checkout(R);
+            Console.WriteLine("Введіть висоту конуса");
+            Console.WriteLine("h=");
+            h = Convert.ToInt32(Console.ReadLine());
+            h = Checkout(h);
+            V = (Math.Pow(R, 2) * h * 3.14) / 3;
+            Console.WriteLine("Відповідь, V={0}", V);
+        }
+    }
+}
